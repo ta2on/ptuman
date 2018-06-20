97e9d2d5b9caedb626f439e01ae5385168901b26
윤태언 <ta2on@naver.com>
2018-06-20 20:53:18 +0900

    test

---
+++ test/src/main/java/test/sample/service/SampleService.java
@@ -0,0 +1,11 @@
+package test.sample.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SampleService {
+
+	List<Map<String, Object>> selectBoardList(Map<String, Object> Map) throws Exception;
+
+}
+
---
+++ test/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<jsp:forward page="/sample/openSampleList.do"/>
+
+<!-- 처음호출하면 여기를 호출하는데 저기로 요청을 보내라고 써있음ㅁ -->
